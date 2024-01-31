cc4de41b338131c1fc6970969dbf77360c29102a
JosephSeong <joseph0903@naver.com>
2024-01-31 17:31:52 +0900

    change userdefaults to coredata

---
+++ ToDo/ToDo/Model/Todo.swift
@@ -0,0 +1,21 @@
+//
+//  Todo.swift
+//  ToDo
+//
+//  Created by Joseph on 12/22/23.
+//
+
+import Foundation
+
+// 할 일 데이터 모델 정의
+struct Todo: Codable {
+    var id: Int
+    var title: String
+    var isCompleted: Bool
+}
+
+// 카테고리와 해당 카테고리에 속하는 할 일 목록을 담는 데이터 모델 정의
+struct CategoryWithTask: Codable{
+    var category: String
+    var tasks: [Todo]
+}
---
+++ ToDo/ToDo/View/VC/ProfileViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ProfileViewController.swift
+//  ToDo
+//
+//  Created by Joseph on 1/31/24.
+//
+
+import UIKit
+import SnapKit
+
+class ProfileViewController: UIViewController {
+
+    private let viewModel = ProfileViewModel()
+
+    //
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        return label
+    }()
+
+    private let ageLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+        return label
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        setupUI()
+        fetchUserProfile()
+    }
+
+    private func setupUI() {
+        view.addSubview(nameLabel)
+        view.addSubview(ageLabel)
+
+        nameLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.centerY.equalToSuperview().offset(-30)
+        }
+
+        ageLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(nameLabel.snp.bottom).offset(10)
+        }
+    }
+
+    private func fetchUserProfile() {
+        viewModel.fetchUserProfile {
+            self.updateUI()
+        }
+    }
+
+    private func updateUI() {
+        nameLabel.text = "이름: \(viewModel.userName)"
+        ageLabel.text = "나이: \(viewModel.userAge)"
+    }
+}
